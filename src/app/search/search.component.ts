6fdaf46f80806c17bbd5d699fef8b969bf84faca
Christian <cbenseler@gmail.com>
2017-01-13 07:43:41 -0200

    feat(search): basic search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//import { default as cep } from 'cep-promise';
+let cep = require('cep-promise/dist/cep-promise');
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  zipcode:string = ""
+  address:any = null
+
   constructor() { }
 
   ngOnInit() {
+ 
+  }
+
+  onSubmit() {
+  	this.address = null
+  	cep(this.zipcode)
+  	.then( data => {
+  		console.log(data)
+  		this.address = data
+  	})
+  	.catch( err => {
+  		console.log(err)
+  	})
   }
 
 }

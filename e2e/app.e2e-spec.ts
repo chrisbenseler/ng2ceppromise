8eb8436dd988273e9fa94e84c881ba7ef6965bf5
Christian <cbenseler@gmail.com>
2017-01-16 11:19:38 -0200

    test: some e2e basic testt

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,46 @@
-import { Ng2ceppromisePage } from './app.po';
+import { Ng2ceppromisePage } from './app.po'
+import { browser, element, by } from 'protractor'
 
 describe('ng2ceppromise App', function() {
   let page: Ng2ceppromisePage;
 
   beforeEach(() => {
-    page = new Ng2ceppromisePage();
+    page = new Ng2ceppromisePage()
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
-});
+  it('should display message with proper title', () => {
+    page.navigateTo()
+    expect(page.getParagraphText()).toEqual('Consulta CEP')
+  })
+
+  it('should have search form', () => {
+    expect(page.getElementsBySelector('form').isPresent()).toBe(true)
+  })
+
+  it('should fill input with valid zip code', () => {
+    page.getElementsBySelector('form input').sendKeys('04477100')
+  })
+
+  it('should submit form', () => {
+    page.getElementsBySelector('form button').click()
+   	
+  })
+
+  it('should have result', () => {
+  	waitForElementToBePresent(page.getElementsBySelector("section div ul"))
+  	let address = page.getElementsBySelector("section div ul li:first-child").getText()
+  	expect(address).toBe('Rua Padre Ramon Ortiz')
+  })
+
+})
+
+function waitForElementToBePresent(element){
+
+	browser.wait(function () {
+	return element.isPresent();
+	},60000);
+
+	browser.wait(function () {
+	return element.isDisplayed();
+	},60000);
+};
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,9 @@ export class Ng2ceppromisePage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getElementsBySelector(selector) {
+  	return element(by.css(selector))
+  }
+
 }
